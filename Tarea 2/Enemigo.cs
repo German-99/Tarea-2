2aafead5e69327d8e619000c0ccbc687d24ed42d
anemi <anemi@DESKTOP-45DQHI2>
2018-08-15 20:29:16 -0700

    Add project files.

---
+++ Tarea 2/Enemigo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_2
+{
+    class Enemigo
+    {
+        //Auto-propierties
+        public int PosiciónX { get; set; }
+        public int PosiciónY { get; set; }
+
+        // Propierties
+        private int _PuntosGanados;
+        public int PuntosGanados
+        {
+            get
+            {
+                return _PuntosGanados;
+            }
+            set
+            {
+                if(PuntosGanados < 100)
+                {
+                    PuntosGanados = 100;
+                }
+            }
+
+        }
+        //Funciones
+        public void moverse()
+        {
+
+        }
+        public void disparar()
+        {
+
+        }
+        public void explotar()
+        {
+
+        }
+    }
+}
